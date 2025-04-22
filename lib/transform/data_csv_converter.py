3d3b49cf01147b9b0895bbc5286b909224565a59
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-04-22 13:50:10 +0200

    chore: add data product manifest loader

--- lib/transform/data_csv_converter.py
+++ lib/transform/data_csv_converter.py
@@ -9,7 +9,6 @@ from lib.tracking_decorator import TrackingDecorator
 def convert_data_to_csv(source_path, results_path, clean=False, quiet=False):
     # Iterate over files
     for subdir, dirs, files in sorted(os.walk(source_path)):
-
         # Make results path
         subdir = subdir.replace(f"{source_path}/", "")
         os.makedirs(os.path.join(results_path, subdir), exist_ok=True)
@@ -31,31 +30,63 @@ def convert_file_to_csv(source_path, source_file_path, clean=False, quiet=False)
     else:
         return
 
-    sheets = ["Fallzahlen_2012", "Fallzahlen_2013", "Fallzahlen_2014", "Fallzahlen_2015", "Fallzahlen_2016",
-              "Fallzahlen_2017", "Fallzahlen_2018", "Fallzahlen_2019", "Fallzahlen_2020", "Fallzahlen_2021",
-              "Fallzahlen_2022", "Fallzahlen_2023"]
+    sheets = [
+        "Fallzahlen_2012",
+        "Fallzahlen_2013",
+        "Fallzahlen_2014",
+        "Fallzahlen_2015",
+        "Fallzahlen_2016",
+        "Fallzahlen_2017",
+        "Fallzahlen_2018",
+        "Fallzahlen_2019",
+        "Fallzahlen_2020",
+        "Fallzahlen_2021",
+        "Fallzahlen_2022",
+        "Fallzahlen_2023",
+    ]
 
     # Iterate over sheets
     for sheet in sheets:
         try:
             skiprows = 5
-            names = ["id", "name", "offences", "robbery", "street_robbery_purse_snatching",
-                     "bodily_harm", "dangerous_and_grievous_bodily_harm",
-                     "deprivation_of_liberty_coercion_threat_stalking",
-                     "theft", "motor_vehicle_theft", "theft_from_motor_vehicle", "bicycle_theft",
-                     "residential_burglary",
-                     "fire_offences", "arson",
-                     "damage_to_property", "damage_to_property_by_graffiti",
-                     "narcotics_offences", "kieztaten"]
+            names = [
+                "id",
+                "name",
+                "offences",
+                "robbery",
+                "street_robbery_purse_snatching",
+                "bodily_harm",
+                "dangerous_and_grievous_bodily_harm",
+                "deprivation_of_liberty_coercion_threat_stalking",
+                "theft",
+                "motor_vehicle_theft",
+                "theft_from_motor_vehicle",
+                "bicycle_theft",
+                "residential_burglary",
+                "fire_offences",
+                "arson",
+                "damage_to_property",
+                "damage_to_property_by_graffiti",
+                "narcotics_offences",
+                "kieztaten",
+            ]
             drop_columns = ["name"]
 
-            dataframe = pd.read_excel(source_file_path, engine=engine, sheet_name=sheet, skiprows=skiprows,
-                                      usecols=list(range(0, len(names))), names=names) \
-                .drop(columns=drop_columns, errors="ignore") \
-                .replace("Eso", None) \
-                .dropna() \
-                .astype(int) \
+            dataframe = (
+                pd.read_excel(
+                    source_file_path,
+                    engine=engine,
+                    sheet_name=sheet,
+                    skiprows=skiprows,
+                    usecols=list(range(0, len(names))),
+                    names=names,
+                )
+                .drop(columns=drop_columns, errors="ignore")
+                .replace("Eso", None)
+                .dropna()
+                .astype(int)
                 .assign(id=lambda x: x["id"].astype(str).str.zfill(6))
+            )
 
             # Drop columns that represent a complete district or the complete city
             dataframe = dataframe[~dataframe["id"].str.endswith("0000")]
@@ -64,13 +95,17 @@ def convert_file_to_csv(source_path, source_file_path, clean=False, quiet=False)
             year = sheet.split(sep="_")[1]
             half_year = "00"
             target_file_name = f"{source_file_name[:-5]}-{year}-{half_year}"
-            file_path_csv = os.path.join(source_path, target_file_name, f"{target_file_name}.csv")
+            file_path_csv = os.path.join(
+                source_path, target_file_name, f"{target_file_name}.csv"
+            )
 
             # Check if result needs to be generated
             if clean or not os.path.exists(file_path_csv):
                 # Write csv file
                 if dataframe.shape[0] > 0:
-                    os.makedirs(os.path.join(source_path, target_file_name), exist_ok=True)
+                    os.makedirs(
+                        os.path.join(source_path, target_file_name), exist_ok=True
+                    )
                     dataframe.to_csv(file_path_csv, index=False)
                     if not quiet:
                         print(f"✓ Convert {os.path.basename(file_path_csv)}")
